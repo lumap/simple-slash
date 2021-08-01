b34759393399110f2fe5ef2adb20bb60f8ea880b
Lumap <lumapbk@gmail.com>
2021-08-01 17:30:03 +0200

    h

--- index.js
+++ index.js
@@ -28,7 +28,33 @@ app.get('/manage',async function (req,res){
   if (!req.query.token) {
     return res.render('give_id')
   } else {
-    return res.render('manage',{})
+    //get the bot id by using the token
+    let id;
+    try {
+      id = (await axios({
+        url: "https://discord.com/api/v9/oauth2/applications/@me",
+        headers: {
+          "Authorization": `Bot ${req.query.token}`,
+          "Content-Type": "application/json"
+        }
+      })).data.id
+    } catch (e) {
+      console.log(e)
+      return res.render('errors/404')
+    }
+    //get the bot's slash commands
+    const slash_commands = (await axios({
+      url: `https://discordapp.com/api/v9/applications/${id}/commands`,
+      headers: {
+        "Authorization": `Bot ${req.query.token}`,
+        "Content-Type": "application/json"
+      }
+    })).data
+    return res.render('manage',{
+      id: id,
+      token: req.query.token,
+      commands: slash_commands
+    })
   }
 })
 
