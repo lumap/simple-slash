8bb7dc79a3a4e24a922f792d0fc5461f343dd575
Lumap <lumapbk@gmail.com>
2021-08-01 23:55:43 +0200

    a

--- index.js
+++ index.js
@@ -1,40 +1,54 @@
 var express = require('express');
+var cookieparser = require("cookie-parser")
 var app = express();
 var axios = require('axios')
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
-app.use("/assets",express.static(__dirname + "/assets"));
+app.use("/assets", express.static(__dirname + "/assets"));
+app.use(cookieparser())
 
 // index page
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.render('index');
 });
 
 // about page
-app.get('/about', function(req, res) {
+app.get('/about', function (req, res) {
   res.render('about');
 });
 
-app.get('/facts/cats', async function (req,res){
+app.get('/facts/cats', async function (req, res) {
   const fact = (await axios("https://catfact.ninja/fact")).data.fact
-  res.render('facts/cats',{
+  res.render('facts/cats', {
     fact: fact
   })
 })
 
-app.get('/manage',async function (req,res){
-  if (!req.query.token) {
-    return res.render('give_id')
-  } else {
+app.get("/manage",function (req,res) {
+  res.render('give_token')
+})
+
+app.get("/setcookie",function(req,res){
+  res.cookie("token",req.query.token).send("OK")
+})
+
+app.get("/deletecookie",function(req,res){
+  res.clearCookie("token")
+  res.send("OK")
+})
+
+app.get('/managecommands', async function (req, res) {
+  const token = req.cookies?req.cookies.token:""
+  if (!token) return res.redirect("/manage")
     //get the bot id by using the token
     let id;
     try {
       id = (await axios({
         url: "https://discord.com/api/v9/oauth2/applications/@me",
         headers: {
-          "Authorization": `Bot ${req.query.token}`,
+          "Authorization": `Bot ${token}`,
           "Content-Type": "application/json"
         }
       })).data.id
@@ -46,21 +60,20 @@ app.get('/manage',async function (req,res){
     const slash_commands = (await axios({
       url: `https://discordapp.com/api/v9/applications/${id}/commands`,
       headers: {
-        "Authorization": `Bot ${req.query.token}`,
+        "Authorization": `Bot ${token}`,
         "Content-Type": "application/json"
       }
     })).data
-    return res.render('manage',{
+    return res.render('manage', {
       id: id,
-      token: req.query.token,
+      token: token,
       commands: slash_commands
     })
-  }
 })
 
 
 
-app.get('*', function(req, res){
+app.get('*', function (req, res) {
   res.status(404).render('errors/404')
 });
 
